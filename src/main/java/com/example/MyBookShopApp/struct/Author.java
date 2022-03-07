0e6f65cae123feaa6994bd91965570d152a950b0
Sianool <antonantilevskiy@mail.ru>
2022-03-07 15:27:31 +0300

    skillbox module 5 homework, fix#1

---
+++ src/main/java/com/example/MyBookShopApp/struct/Author.java
@@ -0,0 +1,60 @@
+package com.example.MyBookShopApp.struct;
+
+import com.example.MyBookShopApp.struct.book.Book;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+public class Author{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> bookList = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+}
---
+++ src/main/java/com/example/MyBookShopApp/struct/book/Book.java
@@ -0,0 +1,96 @@
+package com.example.MyBookShopApp.struct.book;
+
+
+import com.example.MyBookShopApp.struct.Author;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+    private String title;
+    private String priceOld;
+    private String price;
+
+    @Transient
+    private Boolean isSale;
+    @Transient
+    private Integer discount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPriceOld() {
+        return priceOld;
+    }
+
+    public void setPriceOld(String priceOld) {
+        this.priceOld = priceOld;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Boolean isSale() {
+        return isSale;
+    }
+
+    public void setSale(Boolean sale) {
+        isSale = sale;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author=" + author +
+                ", title='" + title + '\'' +
+                ", priceOld='" + priceOld + '\'' +
+                ", price='" + price + '\'' +
+                ", isSale=" + isSale +
+                ", discount=" + discount +
+                '}';
+    }
+}
